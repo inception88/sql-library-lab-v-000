0b2fafa777f4fe769e36e25a2fb87ca145ed8119
Benjamin Jones <bljones36@gmail.com>
2018-11-03 13:43:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  JOIN subgenres
+  LEFT JOIN subgenres
   ON authors.id  = subgenres.author_id"
 end
 
