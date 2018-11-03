900d8411424d9634da812656d98609e7ca3ec0b6
Benjamin Jones <bljones36@gmail.com>
2018-11-03 13:48:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,8 +22,10 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  LEFT JOIN subgenres
-  ON authors.id = subgenres.id"
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON series.subgenre_id = subgenres.name"
 end
 
 def select_series_title_with_most_human_characters
