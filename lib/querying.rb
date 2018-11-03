a1edb035e8505cbe958426a3baaa5b8bc9694762
Benjamin Jones <bljones36@gmail.com>
2018-11-03 13:40:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,8 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species)
   FROM characters
-  GROUP BY characters.species"
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) DESC LIMIT 2"
 end
 
 def select_name_and_series_subgenres_of_authors
