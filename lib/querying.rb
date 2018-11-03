191274b11fffb58f5646e680322ca4f93415b742
Benjamin Jones <bljones36@gmail.com>
2018-11-03 13:26:10 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year
+  "SELECT books.title, books.year, books.series_id
   FROM books
   ORDER BY books.year
   GROUP BY books.series_id"
