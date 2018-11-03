d3c511860d5127a2b6319cf2d2af456006c40eca
Benjamin Jones <bljones36@gmail.com>
2018-11-03 13:27:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year, books.series_id
   FROM books
-  ORDER BY books.year"
-#  GROUP BY books.series_id"
+  ORDER BY books.year
+  WHERE books.series_id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
